d2ca0913bfc8ee5fce5ac420a2d46c843eee266c
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-09 15:36:23 -0400

    Filled out typeDefs

--- index.js
+++ index.js
@@ -98,8 +98,24 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+  }
+
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
