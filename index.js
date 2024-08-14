029b59e0ebe84189387da4aef4c9b0893cf5757b
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-14 16:16:34 -0400

    Implemented me query

--- index.js
+++ index.js
@@ -199,6 +199,9 @@ const resolvers = {
 
       return Book.find(query).populate('author')
     },
+    me: (root, args, context) => {
+      return context.currentUser
+    },
   },
   Author: {
     bookCount: async root => {
@@ -301,6 +304,14 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
